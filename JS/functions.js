6fb4a286890ef0004a5f66d728da0161b534fef1
Geovanny <geoconsultores.eu@gmail.com>
2021-02-28 00:38:41 -0500

    Operaciones con variables y concatenacion

--- JS/functions.js
+++ JS/functions.js
@@ -9,3 +9,11 @@ alert(nombre);
 alert(5+4*8);
 
 alert((5+4)*8);
+
+var precioUnidad, cantidad, total;
+
+precioUnidad = 100;
+cantidad = 5;
+total = precioUnidad * cantidad;
+
+alert("El valor total de la compra es: " + "$ " + total);
